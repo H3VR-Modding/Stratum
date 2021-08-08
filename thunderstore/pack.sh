686f0b984629a76a2a3410285ed73132f29eb67c
Ash <ashe.null@protonmail.ch>
2021-08-08 03:01:02 +0000

    infer package file name from manifest

--- thunderstore/pack.sh
+++ thunderstore/pack.sh
@@ -2,16 +2,20 @@
 shopt -s extglob
 
 MANIFEST=manifest.json
+cd $(dirname $0)
+
+raw=$(jq -r '.author, .name' "$MANIFEST")
+author=$(echo "$raw" | sed -n '1 p')
+name=$(echo "$raw" | sed -n '2 p')
 version=$(dotnet minver --tag-prefix v | sed 's/\([0-9.]*\).*/\1/')
 
-cd $(dirname $0)
 mkdir -p out
 
 pushd raw
 mkfifo "$MANIFEST"
 
 jq --compact-output --arg version "$version" '.version_number = $version' "../$MANIFEST" > "$MANIFEST" &
-zip -FS9r --fifo "../out/Stratum-Stratum-$version.zip" .
+zip -FS9r --fifo "../out/$author-$name-$version.zip" .
 
 rm "$MANIFEST"
 popd
