da71563d8f790dfd03c1f74a21b355e8960a8669
Ash <ashe.null@protonmail.ch>
2021-08-21 04:00:16 +0000

    add docs for collection types

--- Stratum/Helpers/ReadOnlyList.cs
+++ Stratum/Helpers/ReadOnlyList.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 
 namespace Stratum
 {
-	public class ReadOnlyList<T> : IReadOnlyList<T>
+	/// <summary>
+	///		An object that implements <see cref="IReadOnlyList{T}"/>
+	/// </summary>
+	/// <typeparam name="T">The type of the items</typeparam>
+	public sealed class ReadOnlyList<T> : IReadOnlyList<T>
 	{
+		/// <summary>
+		///		Constructs an instance of <see cref="ReadOnlyList{T}"/>
+		/// </summary>
+		/// <param name="list">The list to read from</param>
 		public ReadOnlyList(IList<T> list)
 		{
 			_list = list;
@@ -12,10 +20,13 @@ namespace Stratum
 
 		private readonly IList<T> _list;
 
+		/// <inheritdoc cref="IReadOnlyCollection{T}.Count"/>
 		public int Count => _list.Count;
 
+		/// <inheritdoc cref="IReadOnlyList{T}.this"/>
 		public T this[int index] => _list[index];
 
+		/// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>
 		public IEnumerator<T> GetEnumerator()
 		{
 			return _list.GetEnumerator();
